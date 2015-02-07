fd8c7a463943503ac783f81cd29df64dbbfa74ad
Dustin Dowell <dustindowell22@gmail.com>
2015-02-06 19:53:09 -0600

    1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,5 +2,8 @@ Changelog
 =========
 
 **Febuary 6, 2015**
++ 1.0.1
+  + Completely removed the CodeMirror blinking
+
 + 1.0.0
   + Commited Project
--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 define(function(require, exports, module) {
   var ExtensionUtils = brackets.getModule('utils/ExtensionUtils'),
-      AppInit = brackets.getModule("utils/AppInit");
+      AppInit = brackets.getModule("utils/AppInit"),
+      CodeMirror = brackets.getModule("thirdparty/CodeMirror2/lib/codemirror");
 
   AppInit.appReady(function () {
     ExtensionUtils.loadStyleSheet(module, 'main.less');
+    CodeMirror.defaults.cursorBlinkRate = 0;
   });
 });
